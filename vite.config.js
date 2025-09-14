8243a76ee9dda979ac98013c50ea676e8640deb7
themindlocksyndicate <mindlocksyndicate@proton.me>
2025-09-14 12:17:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'node:path'
 const base = process.env.VITE_BASE || '/'
 
 export default defineConfig({
-  base,
+  base: '/TMLS_playcards_viewer/',
   server: { port: 5173 },
   resolve: { alias: { '@': path.resolve(__dirname, './src') } }
 })
